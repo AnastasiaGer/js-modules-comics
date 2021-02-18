ce434b7abc63c899de130eb53174870b55310fa8
AnastasiaGer <aagermanova@gmail.com>
2021-02-17 16:58:04 -0800

    api

--- src/index.js
+++ src/index.js
@@ -1,1 +1,9 @@
-console.log('Hello');
+import 'regenerator-runtime/runtime';
+
+import {API_URL, URL_COMICS} from './const/api'
+import {getDataApi} from './utils/getData.Api'
+
+(async () => {
+  const data = await getDataApi.getData(API_URL + URL_COMICS);
+  console.log('Result:', data)
+})();
